d85b5ae55bb50f553f556d785cad50237c6c3397
AkramSultani <akram.nadri@gmail.com>
2022-06-14 17:33:12 -0400

    chaining functions with andThen()

--- javafunctional/src/main/java/functionalinterface/_Function.java
+++ javafunctional/src/main/java/functionalinterface/_Function.java
@@ -3,14 +3,20 @@ package functionalinterface;
 import java.util.function.Function;
 
 public class _Function {
-	
+
 	public static void main(String[] args) {
-//		int increment = incrementByOne(0);
-//		System.out.println(increment);
-		
+		//		int increment = incrementByOne(0);
+		//		System.out.println(increment);
+
 		// calling the function, apply takes in the argument
 		int incrementFunction = incrementByOneFunction.apply(4);
-	System.out.println(incrementFunction);
+		System.out.println(incrementFunction);
+
+		int multiplyFunction = multiplyBy10.apply(10);
+		System.out.println(multiplyFunction);
+
+		int addThenMultiple = addByOneThenMultiplyBy10.apply(5);
+		System.out.println(addThenMultiple);
 	}
 
 	// New way of creating function - functional style - same as below
@@ -19,11 +25,19 @@ public class _Function {
 	// number = parameter
 	static Function<Integer, Integer> incrementByOneFunction 
 	= number -> number + 1; 
-	
-	
+
+	// Function that takes in one argument and multiplies value by 10
+	static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
+
+	// Combine 2 functions with .andThen()
+	// Chain functions with .andThen()
+	static Function<Integer, Integer> addByOneThenMultiplyBy10 = 
+			incrementByOneFunction.andThen(multiplyBy10);
+
+
 	// Original function
-//	static int incrementByOne(int number) {
-//		return number + 1;
-//	}
+	//	static int incrementByOne(int number) {
+	//		return number + 1;
+	//	}
 
 }
