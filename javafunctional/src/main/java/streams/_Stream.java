83b06d69f3804d164f9dc3d2036b8ad2419ebe33
AkramSultani <akram.nadri@gmail.com>
2022-06-15 17:49:29 -0400

    Streams - allMatch, noneMatch, anyMatch

--- javafunctional/src/main/java/streams/_Stream.java
+++ javafunctional/src/main/java/streams/_Stream.java
@@ -21,33 +21,54 @@ public class _Stream {
 				new Person("Alex", Gender.MALE),
 				new Person("Bob", Gender.NONBINARY),
 				new Person("Alice", Gender.FEMALE));
-		
-		
+
+
 		// Begin a stream by selecting some sort of list
 		// Collects and prints all genders, removing duplicates in people List
-		Set<Gender> genders = people.stream()
-				.map(person -> person.gender) // transform, creates a list of gender
-				.collect(Collectors.toSet()); // collect to a set, remove duplicates
-		
-		System.out.println(genders);
+		//		Set<Gender> genders = people.stream()
+		//				.map(person -> person.gender) // transform, creates a list of gender
+		//				.collect(Collectors.toSet()); // collect to a set, remove duplicates
 
-		
+		//		System.out.println(genders);
+
+
 		// Collects and prints all names in people List
-		Set<String> names = people.stream()
-				.map(person -> person.name) // transform, creates a list of gender
-				.collect(Collectors.toSet());
-		
-		System.out.println(names);
-		
-		// Will print int value of each name
-		 people.stream()
-				.map(person -> person.name)
-				.mapToInt(name -> name.length())
-				.forEach(name -> System.out.println(name));
+		//		Set<String> names = people.stream()
+		//				.map(person -> person.name) // transform, creates a list of gender
+		//				.collect(Collectors.toSet());
 
+		//		System.out.println(names);
+
+		// Will print int value of each name
+		//		 people.stream()
+		//				.map(person -> person.name)
+		//				.mapToInt(name -> name.length())
+		//				.forEach(name -> System.out.println(name));
+
+
+		// ********** More on streams **********
+
+		// returning boolean value if all person in people list equals to female
+//		boolean isFemaleOnly = people.stream()
+//				.allMatch(person -> person.gender == Gender.FEMALE);
+//
+//		System.out.println(isFemaleOnly);
+		
+		// will return true if we have atleast one female
+//		boolean isFemale = people.stream()
+//				.anyMatch(person -> person.gender == Gender.FEMALE);
+//
+//		System.out.println(isFemale);
+		
+		// return true if no matches are found
+		// return false if match is found
+		boolean checkGender = people.stream()
+				.noneMatch(person -> person.gender == Gender.NONBINARY);
+
+		System.out.println(checkGender);
 	}
-	
-	
+
+
 
 	static class Person {
 		// class variables - private variables only this class has access
