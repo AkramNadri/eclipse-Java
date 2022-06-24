63407ad67809ef279ed148b56f0cb759f6fb4cdf
AkramSultani <akram.nadri@gmail.com>
2022-06-24 17:38:24 -0400

    Spring project

---
+++ springProject/src/springProject/DrawingApp.java
@@ -0,0 +1,35 @@
+package springProject;
+
+import javax.naming.Context;
+
+import org.apache.log4j.lf5.util.Resource;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+	
+	
+	public static void main(String[] args) {
+		
+		// initialize and object using Triangle class
+//		Triangle triangle = new Triangle();
+		
+		// Instantiate BeanFactory
+		// Using Xml BeanFactory reads the configurations of the bean from spring.xml
+		ApplicationContext context = new ClassPathXmlApplicationContext("/Users/ak/Documents/eclipse-workspace1/springProject/src");
+		
+		// Here we are asking the BeanFactory to retrieve the specified bean
+		// we use getBean method and pass the id of the bean
+		// Storing getBean into an object variable and since its an object
+		// we need to cast it using (Triangle).
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		
+//		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
---
+++ springProject/src/springProject/Triangle.java
@@ -0,0 +1,9 @@
+package springProject;
+
+public class Triangle {
+	
+	public void draw() {
+		System.out.println("Triangle drawn");
+	}
+
+}
